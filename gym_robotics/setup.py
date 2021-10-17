fc14c89bb8d06ef9ccdb306e045a4210e7472139
Srini-Rohan <76437900+Srini-Rohan@users.noreply.github.com>
2021-10-17 13:57:17 +0530

    Update setup.py

--- gym_robotics/setup.py
+++ gym_robotics/setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(name='self_balance_bot',
       version='0.0.1',
-      install_requires=['gym', 'pybullet']
+      install_requires=['gym', 'pybullet', 'tensorflow']
 )
 
